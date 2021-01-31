1be36006f214419a5de829591c8fc2e07b3e5492
student <hectormrg0823@gmail.com>
2021-01-31 15:18:18 +0000

    add new files

---
+++ src/test/java/com/demo4_1/sl/demo4/Demo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo4_1.sl.demo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
